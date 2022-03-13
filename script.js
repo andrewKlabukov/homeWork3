ea38be6f8ab9a401347a2862c132f551cd381c58
Andrew Klabukov <Andrew.klabukov@yandex.ru>
2022-03-13 14:35:53 +0400

    change js file

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
+// Задание 1 метод filter
 
+function myFilter(arr, callback) {
+  const filtered = [];
+  
+  for(let i = 0; i < arr.length; i++) {
+    if(callback(arr[i], i, arr)) {
+      filtered.push(arr[i])
+    }
+  }
+  return filtered
+}
+
+// Задание 2 метод map
+
+function myMap(arr, callback) {
+  const mapped = []
+  for(let i = 0; i < arr.length; i++) mapped.push(callback(arr[i], i, arr))
+  return mapped
+}
+
+// Задание 3 метод reduce
+
+function myReduce(arr, callback, initial) {
+  let accum = initial;
+  for(let i = 0; i < arr.length; i++) {
+      accum = callback(accum, arr[i], i, arr);
+  }
+  return accum;
+}
